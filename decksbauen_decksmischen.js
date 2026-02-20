da18169761a1881189ea7025257eb4a14d73ec7e
Volverinlg <volverin.justin@gmail.com>
2026-02-20 10:36:42 +0100

    Add files via upload

---
+++ decksbauen_decksmischen.js
@@ -0,0 +1,73 @@
+//---------------------------------------------------------------------------------
+// Made from Justin Görtz
+// Debugging durch Chatgpt
+'use strict'
+let deck1 ;
+let deck2 ;
+window.onload = function() {
+		baueDeck1() ;
+		baueDeck2() ;
+		mischeDeck1() ;
+		mischeDeck2() ;
+		//austeilen() ;
+		
+}
+
+
+// Nordisches Array
+function baueDeck1() {
+	let Playerone = ['Odin' , 'Thor' , 'Frigg' , 'Hel' , 'Freya' , 'Skadi' , 'Loki' , 
+					'Jack o Lantern' , 'Pixi' , 'Hochpixi' , 'Jackfrost' , 'Kelpie' , 'Werewolf' , 'Zyklop' , 'Valkyre' , 'Typhon' , 'Pegasus' , 
+					'Hydra' , 'Zentaure' , 'Minotaure' , 'Sphinx' , 'Chimäre' , 'Greif' , 'Unicorn' , 'Harpies' , 'Ceberus' , 
+					'Gungir' , 'Gleipnir' , 'Dreizack des Poseidon' , 'Medusa' , 'Aigis' , 'Mjölnir']
+	deck1 = [] ;
+		for(let i = 0; i < Playerone.length; i++) {
+			deck1.push(Playerone[i]) ;
+		}
+		console.log(deck1) ;
+}
+
+
+
+// Griechisches  Array
+function baueDeck2() {
+	let Playertwo = ['Zeus' , 'Baldur' , 'Hera' , 'Hades' , 'Aphrodite' , 'Artemis' , 'Hermes' ,
+					'Jack o Lantern' , 'Pixi' , 'Hochpixi' , 'Jackfrost' , 'Kelpie' , 'Werewolf' , 'Zyklop' , 'Valkyre' , 'Typhon' , 'Pegasus' , 
+					'Hydra' , 'Zentaure' , 'Minotaure' , 'Sphinx' , 'Chimäre' , 'Greif' , 'Unicorn' , 'Harpies' , 'Ceberus' , 
+					'Gungir' , 'Gleipnir' , 'Dreizack des Poseidon' , 'Medusa' , 'Aigis' , 'Mjölnir']
+	deck2 = [] ;
+		for(let i = 0; i < Playertwo.length; i++) {
+			deck2.push(Playertwo[i]) ;
+		}
+		console.log(deck2) ;
+}
+
+function mischeDeck1() {
+	for(let i = 0; i < deck1.length; i++) {
+		let j = Math.floor(Math.random() * deck1.length) ;
+		let temp = deck1[i] ;
+		deck1[i] = deck1[j] ;
+		deck1[j] = temp ;
+	}
+	console.log(deck1) ;
+}
+
+function mischeDeck2() {
+	for(let i = 0; i < deck2.length; i++) {
+		let j = Math.floor(Math.random() * deck2.length) ;
+		let temp = deck2[i] ;
+		deck2[i] = deck2[j] ;
+		deck2[j] = temp ;
+	}
+	console.log(deck2) ;
+}
+// TODO ist nicht ganz fertig
+//function austeilen(karten, Spielfeld) {
+//	for(let motiv of karten) {
+//		let bild = document.createElement('img') ;
+//		bild.src = 'media/bilder/back.png' ;
+//		bild.alt = motiv ;
+//		Spielfeld.appendChild(bild) ;
+//	}
+//}
+//---------------------------------------------------------------------------------
