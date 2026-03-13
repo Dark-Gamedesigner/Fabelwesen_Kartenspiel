5de6bc16cc6d90767757c0a737f55ee959dbe88f
Dark-Gamedesigner <Feurigesdunkel29@gmail.com>
2026-03-13 14:35:46 +0100

    Add files via upload

---
+++ AB0000deckbauen.js
@@ -0,0 +1,120 @@
+//---------------------------------------------------------------------------------
+//erweiterungen Vogel: line 7 geändert zu window.addEventListener; line 13 geändert zu kartendecks()
+'use strict'
+let deck1 ;
+let deck2 ;
+let hand1 = [] ;
+let hand2 = [] ;
+// Made by Justin Görtz
+function kartendecks() {
+		baueDeck1() ;
+		baueDeck2() ;
+		mischeDeck1() ;
+		mischeDeck2() ;
+		/* ---- von Kruse > ----*/
+		handStart1();
+		handStart2();
+		const feld = document.getElementById( "Handkarten" ) ;
+		 seeHand(hand1, feld) ;	// ACHTUNG! Das ist vorerst nur da damit man was sehen kann!!!
+		/* ---- < von Kruse ----*/
+}
+
+// Made by Justin Görtz
+// Nordisches Array
+function baueDeck1() {//erweiterung by Vogel
+	let Playerone = [Odin , Thor , Frigg , Hel , Freya , Skadi , Loki ,
+					JackOLantern , Pixi , Hochpixi , Ratatoskr , Kelpie , Werwolf , Zyklop , Valkyre , Medusa , Pegasus , 
+					Hydra , Zentaure , Minotaure , Sphinx , Chimäre , Greif , Einhorn , Harpyie , Zerberus , 
+					Gleipnir , Dreizack , Aigis , Mjölnir] ;
+	deck1 = [] ;
+		for(let i = 0; i < Playerone.length; i++) {
+			deck1.push(Playerone[i]) ;
+		}
+}
+
+
+// Made by Justin Görtz
+// Griechisches  Array
+function baueDeck2() {//erweiterung by Vogel
+	let Playertwo = [Zeus , Poseidon , Hera , Hades , Aphrodite , Artemis , Hermes ,
+					JackOLantern , Pixi , Hochpixi , Ratatoskr , Kelpie , Werwolf , Zyklop , Valkyre , Medusa , Pegasus , 
+					Hydra , Zentaure , Minotaure , Sphinx , Chimäre , Greif , Einhorn , Harpyie , Zerberus , 
+					Gleipnir , Dreizack , Aigis , Mjölnir] ;
+	deck2 = [] ;
+		for(let i = 0; i < Playertwo.length; i++) {
+			deck2.push(Playertwo[i]) ;
+		}
+}
+// Made by Justin Görtz
+function mischeDeck1() {
+	for(let i = 0; i < deck1.length; i++) {
+		let j = Math.floor(Math.random() * deck1.length) ;
+		let temp = deck1[i] ;
+		deck1[i] = deck1[j] ;
+		deck1[j] = temp ;
+	}
+}
+// Made by Justin Görtz
+function mischeDeck2() {
+	for(let i = 0; i < deck2.length; i++) {
+		let j = Math.floor(Math.random() * deck2.length) ;
+		let temp = deck2[i] ;
+		deck2[i] = deck2[j] ;
+		deck2[j] = temp ;
+	}
+}
+
+//---------------------------------------------------------------------------------
+
+/* ---- von Kruse > ----*/
+function draw( deck, hand ) {
+	const karte = deck.pop() ;	// oberste Karte ziehen
+	hand.push( karte ) ;		// Karte in Hand schieben
+	return karte ;
+}
+
+function handStart1() {
+	for( let i = 0; i < 5; i++) {	// beim Start 5 Karten ziehen
+		draw( deck1, hand1 ) ;		// vom Deck1 auf die Hand1
+	}
+}
+
+function handStart2() {
+	for( let i = 0; i < 5; i++) {	// beim Start 5 Karten ziehen
+		draw( deck2, hand2 ) ;		//vom Deck2 auf die Hand2
+	}
+}
+function seeHand( hand, feld ) {
+	feld.innerHTML = "" ;									// Hand leeren
+	for ( const karte of hand ) {							// Jede Karte durchgehen
+		const bild = document.createElement( "img" ) ;		// Bild erstellen
+		bild.src = "./media/karten/" + karte + ".png" ;		// Bildpfad erstellen
+		feld.appendChild( bild ) ;							// Fügt das erstellte ins Feld
+	}						
+}
+/* ---- < von Kruse ----*/
+//---------------------------------------------------------------------------------------------------------------------------------
+// Geschrieben von André Boden 
+	var Orte = ['Deck', 'Friedhof', 'AngriffPlayer01', 'AngriffPlayer02', 'FeldPlayer01', 'FeldPlayer02', 'Handkarten'] ;
+	
+																				   
+function ablegen() {
+	const spielen = document.querySelectorAll('.aktion');
+	aktion.forEach(aktion => {
+		spielen.addEventListener('click', function(e) {
+			hinlegen()
+	hinlegen()
+		}) ;
+	}) ;
+}
+
+
+function hinlegen(Ort, quelle) {
+	let karte = document.createElement('img') ;
+		karte.src= quelle ;
+        karte.className = 'ruecken' ;
+        Ort.appendChild(karte) ;
+}
+
+
+
---
+++ AB0000myth_wars.js
@@ -0,0 +1,211 @@
+//made by Tim Vogel
+//debugging via Firefox Browser Konsole + Google
+'use strict';
+
+window.addEventListener('load', gameStart);
+
+var PunktePlayer1 = null;
+var PunktePlayer2 = null;
+var FirstTurnPlayer1 = false;
+var FirstTurnPlayer2 = false;
+var isTurnPlayer1 = false;
+var isTurnPlayer2 = false;
+var TurnIsSkipped = false;
+var Player1Lost = false;
+var Player2Lost = false;
+var BothPlayersLoose = false;
+var FehlerImSpiel = false;
+var Runden = 0;
+const hand = document.getElementById('Handkarten')
+const deck = document.getElementById('Deck')
+const friedhof = document.getElementById('Friedhof')
+const angriff01 = document.getElementById('AngriffPlayer01')
+const angriff02 = document.getElementById('AngriffPlayer02')
+const feld01 = document.getElementById('FeldPlayer01')
+const feld02 = document.getElementById('FeldPlayer02')
+
+const spielen = document.querySelectorAll('.aktion');
+
+function gameStart() {
+	
+	hinlegen(document.getElementById('Deck'), 'pictures/Karten_Ruecken.png') ;
+	isTurnPlayer1 = true;
+	kartendecks();//zu finden in deckbauen.js
+	runInitialTurn();
+	
+}
+
+/*spielen.forEach(aktion => {
+	aktion.addEventListener('click', function(e) {
+}
+
+hand.addEventListener('click', ablegen)             ;
+	deck.             ;
+	friedhof.addEventListener('click', ablegen)         ;
+	angriff01.addEventListener('click', ablegen)  ;
+	angriff02.addEventListener('click', ablegen)  ;
+	feld01.addEventListener('click', ablegen)     ;
+	feld02.addEventListener('click', ablegen)     ;*/
+
+function runInitialTurn() {
+	FirstTurnPlayer1 = true;
+	FirstTurnPlayer2 = true;
+	StartNewRound();
+}
+
+function StartNewRound () {
+	Runden++;
+	if (Runden <= 30) {
+		runGame();
+	}else if (PunktePlayer1 < PunktePlayer2) {
+		gameTerminate(0);
+	}else if (PunktePlayer1 > PunktePlayer2) {
+		gameTerminate(1);
+	}else if (PunktePlayer1 == PunktePlayer2){
+		gameTerminate(2);
+	}else {
+		gameTerminate();
+	}
+}
+
+function runGame() {
+	if (FehlerImSpiel === false && BothPlayersLoose === false && Player1Lost === false && Player2Lost === false) {
+		PunkteZaehler();
+	}else {
+		if (BothPlayersLoose === true) {
+			alert("Unentschieden");
+		}else if (Player2Lost === true) {
+			alert("Spieler 1 hat gewonnen");
+		}else if (Player1Lost === true) {
+			alert("Spieler 2 hat gewonnen");
+		}else {
+			alert("Kranplätze müssen verdichtet sein![Du hast ein Leck im Code]");
+		}
+	}
+	document.getElementById('Neuladen').addEventListener('click', ReloadPage);
+}
+
+function PunkteZaehler() {
+	PunktePlayer1 = spieler01.punkte ;
+	PunktePlayer2 = spieler02.punkte ;
+	if (0 <= PunktePlayer1 < 3 && 0 <= PunktePlayer2 < 3) {
+		Turn ();
+	}else if (PunktePlayer1 < 3 && PunktePlayer2 >= 3) {
+		gameTerminate(0);
+	}else if (PunktePlayer1 >= 3 && PunktePlayer2 < 3) {
+		gameTerminate(1);
+	}else if (PunktePlayer1 >= 3 && PunktePlayer2 >= 3) {
+		gameTerminate(2);
+	}else {
+		gameTerminate();
+	}//überprüft Punkte von Spielern, initialisiert Zug wenn beide Spieler keine 3 Punkte, ruft andernfalls Funktion zum Beenden des Spiels auf
+}
+
+function gameTerminate(result) {
+	if (result === 0) {
+		Player1Lost = true;
+		runGame();
+	}else if (result === 1) {
+		Player2Lost = true;
+		runGame();
+	}else if (result === 2) {
+		BothPlayersLoose = true;
+		runGame();
+	}else {
+		FehlerImSpiel = true;
+		runGame();
+	}
+
+//Ergebnisbestimmung und entsprechendes Alert (kann später in andere Funktion übernommen werden)
+}
+
+function Turn() {
+	var Mana = null;
+	var Hand = null;
+	if (isTurnPlayer1 === true) {
+		var Mana = spieler01.speicher ;
+		var Hand = spieler01.hand ;
+		if (Mana < 10) {
+			Mana++;
+		}
+		spieler01.speicher = Mana;
+		if (Hand < 8) {
+			//draw();//zu finden in deckbauen.js (auskommentiert zum testen des codes, weil website in endlosschleife hängt (draw fuktion noch nicht angepasst)
+		}
+		document.getElementById('Zugende').addEventListener('click', TurnSkip);
+		if (TurnIsSkipped === false || FirstTurnPlayer1 === true) {
+			//karten auf spielfeld ablegen (to do)
+			if (FirstTurnPlayer1 === false) {
+				//Angriff();
+				document.getElementById('Angreifen').addEventListener('click', Angriff);
+			}
+		}
+		FirstTurnPlayer1 = false;
+		isTurnPlayer2 = true;
+		isTurnPlayer1 = false;
+		StartNewRound();
+	}else if (isTurnPlayer2 == true) {
+		var Mana = spieler02.speicher
+		var Hand = spieler02.hand
+		if (Mana < 10) {
+			Mana++;
+		}
+		spieler02.speicher = Mana;
+		if (Hand < 8) {
+			//draw();//zu finden in deckbauen.js (auskommentiert zum testen des codes, weil website in endlosschleife hängt (draw fuktion noch nicht angepasst)
+		}
+		document.getElementById('Zugende').addEventListener('click', TurnSkip);
+		if (TurnIsSkipped === false || FirstTurnPlayer2 === true) {
+			//karten auf spielfeld ablegen (to do)
+			if (FirstTurnPlayer2 === false) {
+				//Angriff();
+				document.getElementById('Angreifen').addEventListener('click', Angriff);
+			}
+		}
+		FirstTurnPlayer2 = false;
+		isTurnPlayer1 = true;
+		isTurnPlayer2 = false;
+		StartNewRound();
+	}else {
+		gameTerminate();
+	}
+	
+}
+
+
+//geschrieben von André Boden 
+//abgeändert, angepasst und erweitert von Tim Vogel
+// Schaden tracken
+function Angriff() {
+	var schaden = null ;
+	if(isTurnPlayer1 === true) {
+		schaden = document.getElementById('AngriffPlayer02').getAttribute(HP) - document.getElementById('AngriffPlayer01').getAttribute(Atk);
+		if (schaden > 0) {
+			document.getElementById('AngriffPlayer02').setAttribute(HP, schaden);
+		}else {
+			PunktePlayer1++;
+			//delete.document.getElementById('AngriffPlayer02');//syntax error
+		}
+	}else if(isTurnPlayer2 === true) {
+		schaden = document.getElementById('AngriffPlayer01').HP - document.getElementById('AngriffPlayer02').Atk;
+		if (schaden > 0) {
+			document.getElementById('AngriffPlayer01').setAttribute(HP, schaden);
+		}else {
+			PunktePlayer2++;
+			//delete.document.getElementById('AngriffPlayer01');//syntax error
+		}
+	}else {
+		gameTerminate();
+	}
+}
+//---------------------------------------------------------------------
+
+function TurnSkip() {
+	TurnIsSkipped = true;
+}
+
+function ReloadPage() {
+	window.location.reload();
+}
+
+//------------------------------------------------------------
