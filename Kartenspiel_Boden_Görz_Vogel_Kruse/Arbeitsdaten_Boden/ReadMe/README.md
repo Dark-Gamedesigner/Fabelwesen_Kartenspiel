ae33ce6da23a460a94fe54043e0f1d3a248364a7
Dark-Gamedesigner <Feurigesdunkel29@gmail.com>
2026-03-27 08:59:09 +0100

    Add files via upload

---
+++ Kartenspiel_Boden_Görz_Vogel_Kruse/Arbeitsdaten_Boden/ReadMe/README.md
@@ -0,0 +1,19 @@
+																		# Myth Wars ReadMe 
+	
+	1) Öffne die Index Seite. 
+	
+	
+	2) Drücke auf Spiel Starten.
+	
+	
+	3) Spielablauf:
+		
+		3.1.a) Steuerung erfolgt durch Mausklicks 
+	
+		3.2.a) Die Karten die du auf die Hand bekommst sind anklickbar um sie auf die Bank oder ins Angriffsfeld zu legen.		
+		3.2.b) Klicke eine Karte an, um sie auf die Bank oder ins Angriffsfeld, (durch klick auf besagtes Feld) zu legen.		
+		3.2.c) Im ersten Zug ist Angreifen UNMÖGLICH !!!!
+		3.2.d) Um, ab dem 2. Zug anzugreifen klickst du auf die Karte im Angriffsfeld.
+		3.2.e) Wenn der Angriff erfolgt ist, auf den Button Zug beenden drücken.
+		
+		Danach ist der nächste Spieler dran. 
---
+++ Kartenspiel_Boden_Görz_Vogel_Kruse/Arbeitsdaten_Boden/Scripte/sammlung.js
@@ -0,0 +1,54 @@
+//var schaden = null ;
+//var angriff = null ;
+//var gegnerdef = null ;  
+//var LP = 20 ;
+var klassen = new Angriffskarte(Zerberus Minotaurus)
+
+var LP = 20 ;
+var schaden = null ;
+// Schaden tracken
+function lpabzug() {
+	
+	if(true) {	document.createElementById > Zerberus.HP ;
+			schaden = Minotaurus.Atk - Zerberus.HP;
+			neueLP = LP - schaden			
+	}
+	console.log(neueLP)
+	return(neueLP) ;	
+}
+
+lpabzug()
+
+/*function schadenLP(a) {
+	if(true) {LP > ueberschuss 
+				a = Math.abs(LP - ueberschuss) ;// Schaden abziehen
+	}
+	console.log(a)
+}
+*/
+
+//schadenLP()
+
+// Geschrieben von André Boden 
+	var Orte = ['Deck', 'Friedhof', 'AngriffPlayer01', 'AngriffPlayer02', 'FeldPlayer01', 'FeldPlayer02', 'Handkarten'] ;
+	
+																				   
+function ablegen() {
+	const spielen = document.querySelectorAll('.aktion');
+	aktion.forEach(aktion => {
+		spielen.addEventListener('click', function(e) {
+			hinlegen()
+	hinlegen()
+		}) ;
+	}) ;
+}
+
+
+function hinlegen(Ort, quelle) {
+	let karte = document.createElement('img') ;
+		karte.src= quelle ;
+        karte.className = 'ruecken' ;
+        Ort.appendChild(karte) ;
+		karte.addEventListener('click', e => {aktivekarte = karte }) ;	
+}
+
